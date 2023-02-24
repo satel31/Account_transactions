21bc50490dc21ca7fe777cdf9122568845a04ac5
satel31 <blokhnina.tatiana@yandex.ru>
2023-02-24 22:08:38 +0300

    Added function sort and reformat_data and test for it

--- code/main.py
+++ code/main.py
@@ -1,6 +1,7 @@
-from utils import get_data
+from utils import get_data, sort, reformat_data
 
 PATH = 'data.json'
 
 text = get_data(PATH)
-
+reformatted_text = reformat_data(text)
+sorted_text = sort(reformatted_text)
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -7,3 +7,12 @@ def test_get_data():
     assert utils.get_data('../code/test.json') == [{'a number': ['a', 'number'], 'is': 'is', 'one': 1}]
     with pytest.raises(FileNotFoundError):
         utils.get_data('name.json')
+
+def test_sort():
+    assert utils.sort([{"date": "2019-08-26 10:50"}, {"date": "2022-09-11 17:30"}, {"date": "2019-12-03 04:27"}]) == [{"date": "2022-09-11 17:30"}, {"date": "2019-12-03 04:27"}, {"date": "2019-08-26 10:50"}]
+    with pytest.raises(ValueError):
+        utils.sort([{"date": "2019-08-26 10:"}, {"date": "2022-09-11 17:30"}, {"date": "2019-12-03 04:27"}])
+
+def test_reformat_data():
+    assert utils.reformat_data([{"date": "2019-08-26T10:50:58.294041"}]) == [{"date": "2019-08-26 10:50"}]
+    assert utils.reformat_data([{"not_date": "2019-08-26T10:50:58.294041"}]) == [{"not_date": "2019-08-26T10:50:58.294041"}]
